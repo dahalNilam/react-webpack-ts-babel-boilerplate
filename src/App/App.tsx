7d7555d008e05dae63615261af55051b9029ff82
Nilam Dahal <dahalnilam90@gmail.com>
2019-01-20 10:20:43 -0800

    initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
